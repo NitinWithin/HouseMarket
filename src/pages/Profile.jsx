fef72ca0306a7088ac90e5ad697bc73f1a1eff9b
Nitin Within <nitinwithin123@gmail.com>
2022-05-03 17:35:47 +0530

    Branch 1 (#1)
    * signin and signup completed
    
    * UserProfile in progress

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,92 @@
-import React from 'react'
+import { getAuth, updateProfile } from 'firebase/auth'
+import { useState, useEffect } from 'react'
+import { useNavigate, Link } from 'react-router-dom'
+import { updateDoc, doc } from 'firebase/firestore'
+import { db } from '../firebase.config'
+import { toast } from 'react-toastify'
 
 function Profile() {
-  return (
-    <div>Profile</div>
-  )
+  const auth = getAuth()
+
+  const [changeDetails, setChangeDetails] = useState(false)
+
+  const [formdata, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email
+  })
+
+  const { name, email } = formdata
+
+  const navigate = useNavigate()
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value
+    }))
+  }
+
+  const onSubmit = async () => {
+
+    try {
+      if (auth.currentUser.displayName !== name) {
+        await updateProfile(auth.currentUser, {
+          displayName: name
+        })
+
+        const userRef = doc(db, 'users', auth.currentUser.uid)
+        await updateDoc(userRef, {
+          name // Same as name: name 
+
+        })
+      }
+    } catch (error) {
+      toast.error("Could not save the changes. Please try later")
+    }
+  }
+
+  const onLogout = () => {
+    auth.signOut()
+    navigate('/')
+  }
+
+  return <div className="profile">
+    <header className="profileHeader">
+      <p className="pageHeader">My Profile</p>
+      <button type='button' className='logout' onClick={onLogout}></button>
+    </header>
+
+    <main>
+      <div className="profileDetailsHeader">
+        <p className="profileDetailsText">Personal Details</p>
+        <p className="changePersonalDetails" onClick={() => {
+          changeDetails && onSubmit()
+          setChangeDetails((prevState) => !prevState)
+        }}>
+          {changeDetails ? 'save' : 'change'}
+        </p>
+      </div>
+      <div className="profileCard">
+        <form>
+          <input
+            type="text"
+            id='name'
+            className={!changeDetails ? 'profileName' : 'profileNameActive'}
+            disbled={!changeDetails}
+            value={name}
+            onChange={onChange} />
+
+          <input
+            type="text"
+            id='email'
+            className='profileEmail'
+            disbled={true}
+            value={email}
+            onChange={onChange} />
+        </form>
+      </div>
+    </main>
+  </div>
 }
 
 export default Profile
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,8 +1,100 @@
-import React from 'react'
+import { useState } from 'react'
+import { toast } from 'react-toastify'
+import { Link, useNavigate } from 'react-router-dom'
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
+import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
+import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 
 function SignIn() {
+  const [showPassword, setShowPassword] = useState(false)
+  const [formdata, setFormData] = useState({
+    email: '',
+    password: ''
+  })
+  const { email, password } = formdata
+
+  const navigate = useNavigate();
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value
+    }))
+  }
+
+  const onSubmit = async (e) => {
+    e.preventDefault()
+
+    try {
+      const auth = getAuth()
+
+      const userCredentials = await signInWithEmailAndPassword
+        ( auth,
+          email,
+          password)
+
+      if (userCredentials.user) {
+        navigate('/')
+      }
+
+    } catch (error) {
+      toast.error("Bad user credentials")
+    }
+  }
+
   return (
-    <div>Sign In</div>
+    <>
+      <div className='pageContainer'>
+        <header>
+          <p className="pageHeader">Welcome back!</p>
+        </header>
+        <form onSubmit={onSubmit}>
+          <input
+            type="email"
+            className='emailInput'
+            placeholder='Email'
+            id='email'
+            value={email}
+            onChange={onChange}
+          />
+
+          <div className="passwordInputDiv">
+            <input
+              type={showPassword ? 'text' : 'current-password'}
+              className='passwordInput'
+              placeholder='Password'
+              id='password'
+              value={password}
+              onChange={onChange}
+            />
+
+            <img
+              src={visibilityIcon}
+              alt='show password'
+              className="showPassword"
+              onClick={() => setShowPassword((prevState) => !prevState)}
+            />
+          </div>
+          <Link to='/forgot-password' className='forgotPasswordLink'>
+            Forgot Password?
+          </Link>
+
+          <div className="signInBar">
+            <p className="signInText">SIGN IN</p>
+            <button className="signInButton">
+              <ArrowRightIcon fill='#fffff' width={'34px'} height='34px' />
+
+            </button>
+          </div>
+        </form>
+
+        {/* Google Oauth */}
+
+        <Link to={'/sign-up'} className='registerLink'>
+          Sign Up Instead
+        </Link>
+      </div>
+    </>
   )
 }
 
