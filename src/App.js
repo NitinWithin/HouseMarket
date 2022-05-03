f375422f87eb20e97a9c9b069a96e938c73de767
NitinWithin <nitinwithin123@gmail.com>
2022-05-03 18:09:56 +0530

    PrivateRouter completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import { ToastContainer } from "react-toastify"
-import 'react-toastify/dist/ReactToastify.css';
+import 'react-toastify/dist/ReactToastify.css'
+import PrivateRoute from "./components/PrivateRoute"
 import NavBar from "./components/NavBar"
 import Explore from "./pages/Explore"
 import Offers from "./pages/Offers"
@@ -19,12 +20,14 @@ function App() {
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/profile" element={<Profile />} />
+          <Route path="/profile" element={<PrivateRoute />} >
+            <Route path="/profile" element={<Profile />} />
+          </Route>
         </Routes>
-        <NavBar/>
+        <NavBar />
       </Router>
 
-      <ToastContainer/>
+      <ToastContainer />
     </>
   );
 }
